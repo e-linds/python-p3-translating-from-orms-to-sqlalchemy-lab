103359888056f0aff786c2335202b16dace1f9d8
Emmi Lindsey <emmilindsey@gmail.com>
2023-12-20 13:56:46 -0700

    complete lab

--- lib/dog.py
+++ lib/dog.py
@@ -1,22 +1,33 @@
-from models import Dog
+from models import *
 
-def create_table(base):
-    pass
+
+def create_table(Base, engine):
+
+    Base.metadata.create_all(engine)
 
 def save(session, dog):
-    pass
+    if type(dog) is Dog:
+        session.add(dog)
+        session.commit()
 
 def get_all(session):
-    pass
+    all_dogs = session.query(Dog).all()
+    return all_dogs
+    
 
 def find_by_name(session, name):
-    pass
+    dogs = session.query(Dog).filter(Dog.name == name).first()
+    return dogs
 
 def find_by_id(session, id):
-    pass
+    dog = session.query(Dog).filter(Dog.id == id).first()
+    return dog
 
 def find_by_name_and_breed(session, name, breed):
-    pass
+    dog = session.query(Dog).filter(Dog.name == name, Dog.breed == breed).first()
+    return dog
 
 def update_breed(session, dog, breed):
-    pass
+    if type(dog) is Dog:
+        dog.breed = breed
+        session.commit()
